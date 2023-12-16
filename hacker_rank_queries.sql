795d4c507995eeaa9490bc9903c593b8c298d873
sadiqasaikh2023 <sadiqashaikh2023@gmail.com>
2023-12-16 19:12:56 +0530

    added the question for the given sql query

--- hacker_rank_queries.sql
+++ hacker_rank_queries.sql
@@ -3,3 +3,10 @@
 
 SELECT * FROM CITY 
 WHERE ID = 1661;
+
+--question no 2
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+SELECT *
+FROM CITY
+WHERE Population > 100000 AND CountryCode = 'USA';
